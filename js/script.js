1ec0561468952cd47ee1e5680f4fece3ecc7b1b4
Samara Cardoso <samaracardoso@MacBook-Air-de-Samara.local>
2025-05-02 17:51:14 -0300

    mudancas na imagem de fundo e funcao onclick

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function getUserEmailAndShowMsg () {
+
+    let user_email = document.getElementById("email_input").value 
+    alert(`Obrigada! Em breve você recebrá informaçoes no endereço ${user_email}`)
+}
